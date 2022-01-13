5a44afeb4c71f3644dbc2568cfe0d8ff6be89456
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-01-12 22:02:51 -0500

    Attempt five at fixing test_runner

--- test_runner.py
+++ test_runner.py
@@ -1,26 +1,38 @@
 import subprocess
 import sys
 
-_, good_analysis = sys.argv
+import click
+
 base = [sys.executable, "-m"]
 
-print("===== [START PYTEST] =====")
-cmd = [*base, "pytest", "tests/", "-k", "not incompatible_with_mypyc", "--color=yes", "-v"]
-subprocess.run(cmd, check=True)
 
-print("===== [START DIFF-SHADES - ANALYZE NEWLY BUILT WHEEL]")
-cmd = [
-    *base, "diff_shades", "analyze", ".analysis.json",
-    "--repeat-projects-from", good_analysis, "--work-dir", ".cache", "-v"
-]
-subprocess.run(cmd, check=True)
+@click.command()
+@click.argument("good-analysis", type=click.Path(resolve_path=True, exists=True))
+def main(good_analysis: str) -> None:
+    print("===== [PYTEST] =====", flush=True)
+    cmd = [
+        *base, "pytest", "tests/",
+        "-k", "not incompatible_with_mypyc", "--color=yes", "-v"
+    ]
+    subprocess.run(cmd, check=True)
 
-print("===== [START DIFF-SHADES - CHECK FOR FAILED FILES]")
-subprocess.run([*base, "diff_shades", "show-failed", ".analysis.json", "--show-log"])
+    sys.stdout.flush()
+    print("===== [DIFF-SHADES - ANALYZE NEWLY BUILT WHEEL] ===== ", flush=True)
+    cmd = [
+        *base, "diff_shades", "analyze", ".analysis.json",
+        "--repeat-projects-from", good_analysis, "--work-dir", ".cache", "-v"
+    ]
+    subprocess.run(cmd, check=True)
 
-print("===== [START DIFF-SHADES - COMPARE PURE PYTHON & COMPILED RESULTS]")
-cmd = [
-    *base, "diff_shades", "compare",
-    good_analysis, ".analysis.json", "--diff", "--check"
-]
-subprocess.run(cmd, check=True)
+    print("===== [DIFF-SHADES - CHECK FOR FAILED FILES] =====", flush=True)
+    subprocess.run([*base, "diff_shades", "show-failed", ".analysis.json", "--show-log"])
+
+    print("===== [DIFF-SHADES - COMPARE PURE PYTHON & COMPILED RESULTS] ===== ", flush=True)
+    cmd = [
+        *base, "diff_shades", "compare",
+        good_analysis, ".analysis.json", "--diff", "--check"
+    ]
+    subprocess.run(cmd, check=True)
+
+
+main()
