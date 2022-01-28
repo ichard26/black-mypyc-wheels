4e58cf2a48a664ed392e8e06ec4f72b526a5186d
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-01-27 21:21:47 -0500

    Whelp.

--- test_runner.py
+++ test_runner.py
@@ -15,26 +15,24 @@ def main(good_analysis: str) -> None:
         "-k", "not incompatible_with_mypyc", "--color=yes", "-v"
     ]
     subprocess.run(cmd, check=True)
-    if sys.version_info <= (3, 6):
-        return
+    if sys.version_info >= (3, 7):
+        sys.stdout.flush()
+        print("===== [DIFF-SHADES - ANALYZE NEWLY BUILT WHEEL] ===== ", flush=True)
+        cmd = [
+            *base, "diff_shades", "analyze", ".analysis.json",
+            "--repeat-projects-from", good_analysis, "--work-dir", ".cache", "-v"
+        ]
+        subprocess.run(cmd, check=True)
 
-    sys.stdout.flush()
-    print("===== [DIFF-SHADES - ANALYZE NEWLY BUILT WHEEL] ===== ", flush=True)
-    cmd = [
-        *base, "diff_shades", "analyze", ".analysis.json",
-        "--repeat-projects-from", good_analysis, "--work-dir", ".cache", "-v"
-    ]
-    subprocess.run(cmd, check=True)
+        print("===== [DIFF-SHADES - CHECK FOR FAILED FILES] =====", flush=True)
+        subprocess.run([*base, "diff_shades", "show-failed", ".analysis.json", "--show-log"])
 
-    print("===== [DIFF-SHADES - CHECK FOR FAILED FILES] =====", flush=True)
-    subprocess.run([*base, "diff_shades", "show-failed", ".analysis.json", "--show-log"])
-
-    print("===== [DIFF-SHADES - COMPARE PURE PYTHON & COMPILED RESULTS] ===== ", flush=True)
-    cmd = [
-        *base, "diff_shades", "compare",
-        good_analysis, ".analysis.json", "--diff", "--check"
-    ]
-    subprocess.run(cmd, check=True)
+        print("===== [DIFF-SHADES - COMPARE PURE PYTHON & COMPILED RESULTS] ===== ", flush=True)
+        cmd = [
+            *base, "diff_shades", "compare",
+            good_analysis, ".analysis.json", "--diff", "--check"
+        ]
+        subprocess.run(cmd, check=True)
 
 
 main()
